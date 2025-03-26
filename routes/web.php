7a617e3dd3c36f8fc972e338eaa8b82f2493471b
Daniel Carlos <daniel.carlos.net@gmail.com>
2025-03-25 23:26:02 -0300

    feat:breeze authentication

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,25 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
+Route::controller(PageController::class)->group(function () {
+    Route::get('/host', 'host')->name('Page.host');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::controller(PageController::class)->group(function () {
-    Route::get('/host', 'host')->name('Page.host');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
